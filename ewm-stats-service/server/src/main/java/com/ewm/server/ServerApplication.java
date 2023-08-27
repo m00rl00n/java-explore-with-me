bf170d12626b584f7fd46b6c372ca6f293546178
moorloon <moorloon@yandex.ru>
2023-08-28 03:18:39 +0700

    ......еще попытка тест

--- ewm-stats-service/server/src/main/java/com/ewm/server/ServerApplication.java
+++ ewm-stats-service/server/src/main/java/com/ewm/server/ServerApplication.java
@@ -1,11 +1,13 @@
-package ru.practicum;
+package com.ewm.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServerApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
     }
+
 }
