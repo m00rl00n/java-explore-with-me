ece6059bc4168b756283dded2baef3a95e7183b0
moorloon <moorloon@yandex.ru>
2023-08-28 04:14:48 +0700

    ......еще попытка тест 3

--- ewm-stats-service/server/src/main/java/ru/practicum/service/StatsService.java
+++ ewm-stats-service/server/src/main/java/ru/practicum/service/StatsService.java
@@ -2,7 +2,6 @@ package ru.practicum.service;
 
 import model.StatsHitDto;
 import model.StatsResponseDto;
-import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
