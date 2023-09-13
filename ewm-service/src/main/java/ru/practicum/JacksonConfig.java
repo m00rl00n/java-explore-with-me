7bea2cddd0006382fac89ba31c2059dc9cc83cf7
moorloon <moorloon@yandex.ru>
2023-09-13 14:15:57 +0700

    3 этап

---
+++ ewm-service/src/main/java/ru/practicum/JacksonConfig.java
@@ -0,0 +1,19 @@
+package ru.practicum;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.util.StdDateFormat;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.setDateFormat(new StdDateFormat().withColonInTimeZone(true));
+        return objectMapper;
+    }
+}
